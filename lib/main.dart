c19a2de81526ea3e8153316faf0650bbda60a65b
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-07-29 09:05:07 -0400

    Currently working on the slider of the various options

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -29,19 +31,54 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+      padding: const EdgeInsets.all(12.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start ,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Find your', style: GoogleFonts.poppins(fontSize: 14)),
-              Text('favorite plants'),
+              Text('Find your',
+                  style: GoogleFonts.poppins(
+                      fontSize: 15, fontWeight: FontWeight.w500)),
+              Text('favorite plants',
+                  style: GoogleFonts.poppins(
+                      fontSize: 15, fontWeight: FontWeight.w500)),
             ],
           ),
-          Icon(Icons.search)
+          Container(
+              padding: const EdgeInsets.all(6.0),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2.0, color: Colors.grey.shade300),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Icon(Icons.search))
+        ]),
+        SizedBox(height: 12),
+        Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(12)),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('30% Off',
+                            style: GoogleFonts.poppins(
+                                fontSize: 14, fontWeight: FontWeight.w500)),
+                        Text('02 - 03 July',
+                            style: GoogleFonts.poppins(fontSize: 14))
+                      ]),
+                  Image.asset(
+                    "images/pexels-photo-12057742.jpeg",
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.fill,
+                  )
+                ]))
       ]),
-    ]),
-        ));
+    ));
   }
 }
